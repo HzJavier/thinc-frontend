a85b455e043fc79e6776002ada31a2ca354294f4
emmaldon <emmanuelburgos@hotmail.com>
2016-03-12 10:18:15 -0600

    Message

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,15 @@
-console.log('hola');
+
+
+function propExercise(inputObj,property){
+	return inputObj[property];
+
+}
+
+inputObj={
+	siren:"green",
+	cup:"white",
+	desk:"brown"
+}
+
+// propExercise(inputObj,'cup'); //cup
+console.log(inputObj);
