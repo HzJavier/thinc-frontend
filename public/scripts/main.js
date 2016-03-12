e56427441a2157791162d9d0a47b728f2931e7be
Livier Hernandez <livier.hernandez@appco.mx>
2016-03-12 10:44:17 -0600

    Create pseudo class object for a user

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -10,3 +10,14 @@ myObj = {
 };
 
 getProperty(myObj,"property1");
+
+var user = {
+  userName: "User001",
+  email: "user001@hotmail.com",
+  dateOfBirth: "12/12/1990",
+  calculateAge: function(){
+    //function do...
+  }
+};
+
+console.log(user);
