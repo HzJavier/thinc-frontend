73136785931ce9497d7b7817e643bb3930c37ca2
Moises Vera <moyvera@Moisess-MacBook-Pro.local>
2016-03-12 13:49:39 -0600

    end of class, TODO: implement list amazon style

--- public/js/jquery_scripting.js
+++ public/js/jquery_scripting.js
@@ -4,10 +4,31 @@ $(document).ready(function(){
 
   mainWrapper.css('height', wrapperHeight / 2);
 
+  // div to change color
   var firstChild = $('#first-child');
+  // input text, should be validated
   var inputColor = $('#input-color');
+
+  //handling click event
   $('#color-button').click(function(){
+    //change color
     firstChild.css('background-color', inputColor.val());
+  });
+
+
+  // ajax request
+  $.ajax({
+    url: 'data/data.json',
+    dataType: 'json',
+    success: function(data, status){
+      console.log(data);
+      console.log(status);
+      //for each invoice
+      //TODO: implement list amazon style
+      data.invoices.forEach(function(invoice){
+        firstChild.append('<div>' + invoice.client + '</div>');
+      });
+    }
   });
 
 });
