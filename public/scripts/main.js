e37d3270c2f5a6ccd00ce26a6ac70cab699fbc12
HzJavier <javier.hernandez@wizeservices.com>
2016-03-12 09:54:34 -0600

    Explanation of functions and objects
    Also, include example of how to create
    context object variables

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,27 @@
-console.log('hola');
+
+/**
+ * Defining functions
+ */
+function myFunc() {
+  console.log(arguments[2]);
+}
+
+ myFunc("this is arg 1", "this is arg 2", "3", "4");
+
+ /**
+  * Object literal notation
+  */
+var myApp = {
+  myObj: {
+    property1: "some property",
+    property2: "ANOTHER property",
+  },
+  someArray: [1, 2, 3]
+};
+
+console.log(myApp.myObj.property1);
+console.log(myApp.myObj["property2"]);
+
+function getProperty (input, property) {
+  return input[property];
+}
