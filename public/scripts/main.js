50bca274b02cc2d5ac203c9cc69c40e527ca5035
Baruc Espinosa <baruc.sal.espinosa-zarate@hp.com>
2016-03-12 10:06:00 -0600

    Modified file

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,12 @@
-console.log('hola');
+
+var inputObj = {
+	first: "first prop",
+	second: "second prop",
+	third: "third prop"
+};
+
+function getProperty(inputObj, property){
+	console.log(inputObj[property]);
+}
+
+getProperty(inputObj, "first");
