0ce89659ce96f63cf822e0e4a0ecbf7d9bf293e7
HzJavier <javier.hernandez@wizeservices.com>
2016-03-12 12:42:12 -0600

    Implement basic jQuery selectors
    Css and html methods. Also, remove
    unnecessary code on css files.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -105,5 +105,3 @@ moises.sayGoodbye = function () {
 User.prototype.sayHello = function () {
   return "Hello!";
 };
-
-
--- public/scripts/secondary.js
+++ public/scripts/secondary.js
@@ -1,1 +1,13 @@
-var myObj = "some string";
+$(document).ready(function () {
+  $('#main-wrapper').html('Content for wrapper');
+  $('#main-wrapper').css('background-color', '#DDD');
+
+  var bgColor = $('#main-wrapper').css('background-color');
+
+  /**
+   * Make div height to half
+   */
+  var currentHeight = $('#main-wrapper').css('height');
+  $('#main-wrapper').css('height', parseInt(currentHeight)/2);
+   currentHeight = $('#main-wrapper').css('height');
+});
