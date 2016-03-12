e1ac610c39742a30a7939ce59e34b37c230e10ab
Livier Hernandez <livier.hernandez@appco.mx>
2016-03-12 10:06:46 -0600

    Funciones JS

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,12 @@
-console.log('hola');
+console.log('hola')
+
+function getProperty(input, property){
+   return input[property];
+}
+
+myObj = {
+  property1: "hola",
+  property2: "adios"
+};
+
+getProperty(myObj,"property1");
