53a2bfa4150a767aca5f5b6e56896827cc34f634
HzJavier <javier.hernandez@wizeservices.com>
2016-03-12 13:18:12 -0600

    Implement form to change background color of main div

--- public/scripts/secondary.js
+++ public/scripts/secondary.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
   var mainWrapper = $('#main-wrapper');
-
-  mainWrapper.html('Content for wrapper');
   mainWrapper.css('background-color', '#DDD');
 
   var bgColor = mainWrapper.css('background-color');
@@ -22,4 +20,17 @@ $(document).ready(function () {
   console.log('before timeout');
   setTimeout(onTimeout, 4000);
   console.log('after timeout');
+
+  /**
+   * Update background color 
+   */
+
+    // Handle button click event
+    $('#change-color-button').click(function () {
+      // Get color from textfield
+      var newBgColor = $('#color-input').val();
+      // Set new color on background
+      mainWrapper.css('background-color', newBgColor); 
+    });
+
 });
