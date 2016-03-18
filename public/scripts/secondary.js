017c99adc18f470a7b58ea2666e78ce99d21cbf3
Livier Hernandez <livier.hernandez@appco.mx>
2016-03-17 23:50:50 -0600

    Change of background

--- public/scripts/secondary.js
+++ public/scripts/secondary.js
@@ -2,36 +2,40 @@ $('document').ready(function() {
   var mainWrapper = $('#main-wrapper');
   var detailWrapper = $('#detail-wrapper');
 
-  var mainHeight = mainWrapper.css('height');
-
-  mainHeight = mainWrapper.css('height');
-  mainWrapper.css('height',parseInt(mainHeight)/2);
-  mainHeight = mainWrapper.css('height');
-
-  console.log(mainHeight);
-
   $('#button1').click(function() {
     $.getJSON('data/data.json', function(data){
-        //console.log(data);
+        console.log(data);
         mainWrapper.html('');
         var movies = data.movies;
         movies.forEach(function(movies){
           mainWrapper.append('<div><a href="#" id="get-json-'+movies.id+'">'+movies.name+'</a></div>');
         });
     });
+    $('#header-txt').css('font-size','50px');
     mainWrapper.css('width','300px');
     mainWrapper.css('text-align','left');
-    mainWrapper.css('margin','30px 30px 30px 30px');
+    mainWrapper.css('margin-right','50px');
+    mainWrapper.css('margin-left','25px');
+    mainWrapper.css('margin-top','0px');
     detailWrapper.css('display','inline-block');
     mainWrapper.css('display','inline-block');
   });
 
-  $('#get-json-1').click(function() {
-    $.getJSON('data/data.json', function(data){
-        console.log(data);
-        var movieResult = JSON['movies'].filter(function(x){ return x.id == 1; });
-        detailWrapper.append('<div>'+movieResult.name+'</div>');
-    });
-  });
+});
 
+$(document).on('click', '#get-json-1',function() {
+  $.getJSON('data/data.json', function(data){
+      //console.log(data);
+      $('#detail-wrapper').html('');
+      var movies = data.movies;
+      movies.forEach(function (movie) {
+          if(movie.id==1){
+            $('#detail-wrapper').append('<img id="img-detail" src="'+movie.img_url+'" height="400" width="230"></div>');
+            $('#detail-wrapper').append('<div id="movie-name"><p>'+movie.name+'</p></div>');
+            $('#detail-wrapper').append('<div><p>Director: '+movie.director+'</p></div>');
+            $('#detail-wrapper').append('<div><p>Year: '+movie.year+'</p></div>');
+            $('#detail-wrapper').append('<div><p>'+movie.short_description+'</p></div>');
+          }
+      });
+  });
 });
