450231d4f3a889082184afa2bb457ac178b66c98
Livier Hernandez <livier.hernandez@appco.mx>
2016-03-19 10:26:53 -0600

    Angular intro

--- public/scripts/secondary.js
+++ public/scripts/secondary.js
@@ -4,13 +4,26 @@ $('document').ready(function() {
 
   $('#button1').click(function() {
     $.getJSON('data/data.json', function(data){
-        console.log(data);
         mainWrapper.html('');
         var movies = data.movies;
-        movies.forEach(function(movies){
-          mainWrapper.append('<div><a href="#" id="get-json-'+movies.id+'">'+movies.name+'</a></div>');
+        movies.forEach(function(movie,index){
+          var movieItem = $('<div>'+movie.name+'</div>');
+          mainWrapper.append(movieItem);
+
+          movieItem.click(function () {
+            $.getJSON('data/data.json', function(data){
+                $('#detail-wrapper').html('');
+                console.log('click on movie', movie);
+                $('#detail-wrapper').append('<img id="img-detail" src="'+movie.img_url+'" height="400" width="230"></div>');
+                $('#detail-wrapper').append('<div id="movie-name"><p>'+movie.name+'</p></div>');
+                $('#detail-wrapper').append('<div><p>Director: '+movie.director+'</p></div>');
+                $('#detail-wrapper').append('<div><p>Year: '+movie.year+'</p></div>');
+                $('#detail-wrapper').append('<div><p>'+movie.short_description+'</p></div>');
+            });
+          });
         });
     });
+    
     $('#header-txt').css('font-size','50px');
     mainWrapper.css('width','300px');
     mainWrapper.css('text-align','left');
@@ -21,21 +34,4 @@ $('document').ready(function() {
     mainWrapper.css('display','inline-block');
   });
 
-});
-
-$(document).on('click', '#get-json-1',function() {
-  $.getJSON('data/data.json', function(data){
-      //console.log(data);
-      $('#detail-wrapper').html('');
-      var movies = data.movies;
-      movies.forEach(function (movie) {
-          if(movie.id==1){
-            $('#detail-wrapper').append('<img id="img-detail" src="'+movie.img_url+'" height="400" width="230"></div>');
-            $('#detail-wrapper').append('<div id="movie-name"><p>'+movie.name+'</p></div>');
-            $('#detail-wrapper').append('<div><p>Director: '+movie.director+'</p></div>');
-            $('#detail-wrapper').append('<div><p>Year: '+movie.year+'</p></div>');
-            $('#detail-wrapper').append('<div><p>'+movie.short_description+'</p></div>');
-          }
-      });
-  });
 });
