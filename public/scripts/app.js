9c1737f9e6a3557c6c5cb659381800977420638b
HzJavier <javier.hernandez@wizeservices.com>
2016-03-19 10:20:53 -0600

    Implement two way data binding

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,9 +1,12 @@
 var reviewApp = angular.module('reviewApp', []);
 
-reviewApp.controller('BookListCtrl', ['$scope', function ($scope) {
+reviewApp.controller('BookListCtrl', ['$scope', '$http', function ($scope, $http) {
   
   $scope.books = [
     { id: 0, name: 'Javsacript The Good Parts'},
     { id: 1, name: 'Clean Code'}
   ];
+
+  var bestBook = "The martian";
+  $scope.weeklyRecommendation = "Mastering Refactoring";
 }]);
