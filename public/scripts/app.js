0fa6dd8bacad5966f874887af4e0f1364ffcd3ba
Diego Fuentes <DiegoFu@MacBook-Air-de-Diego.local>
2016-03-19 10:33:12 -0600

    Update

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,9 +1,17 @@
 var reviewApp = angular.module('reviewApp', []);
 
-reviewApp.controller('BookListCtrl', ['$scope', function ($scope) {
-  
+reviewApp.controller('BookListCtrl', ['$scope', '$http', function ($scope, $http) {
+
+  $http.get('data/data.json')
+  .sucess(function(data){
+  $scope.books = data;
+});
+
   $scope.books = [
-    { id: 0, name: 'Javsacript The Good Parts'},
+    { id: 0, name: 'Javascript The Good Parts'},
     { id: 1, name: 'Clean Code'}
   ];
+
+  var bestBook = "The Martian";
+  $scope.weeklyRecommendation = "Mastering Refactoring";
 }]);
